39cac024ce20711b5441aee7d8b9c0872636d6ec
Sotatek-DuyNguyen2 <duy.nguyen2@sotatek.com>
2022-11-28 14:29:07 +0700

    01.check astrone install

--- start/contract.js
+++ start/contract.js
@@ -4,5 +4,24 @@ const forwarderOrigin = 'http://localhost:9010'
 
 const initialize = () => {
   //You will start here 
+
+  //Basic Actions Section
+  const onboardButton = document.getElementById('connectButton');
+
+  //Created check function to see if the Astrone extension is installed
+  const isAstroneInstalled = () => {
+    //Have to check the astrone binding on the window object to see if it's installed
+    const {astrone} = window;
+    return Boolean(astrone && astrone.isAstrone);
+  }
+  const AstroneClientCheck = () => {
+    const isInstalled = isAstroneInstalled();
+    if (!isInstalled) {
+      onboardButton.innerText = 'Click here to install Astrone';
+    } else {
+      onboardButton.innerText = 'Connect';
+    }
+  }
+  AstroneClientCheck();
 }
 window.addEventListener('DOMContentLoaded', initialize)
